6434e07a11ce80a1f2c6b739a417380ad783e560
Alec Jandorek <alecjandorek@Alecs-MacBook-Pro.local>
2016-11-17 15:03:38 -0600

    Beta game files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,121 @@
+//Creating a function that doesn't allow jQuert to run until the HTML has loaded
 $(document).ready(function(){
-	var attackPower = 8
+	
+	//Setting an object containing our characters and all their base stats
 	var characters = {
-		reaper: { name: "Reaper", health: 120, counter: 25},
-		winston: { name: "Winston", health: 180, counter: 15},
-		hanzo: { name: "Hanzo", health: 150, counter: 20 },
-		soldier: { name: "Solider 76", health: 100, counter: 30 }
+		reaper: { name: "Reaper", health: 100, counter: 25, image: "assets/images/reaper.png", attack: 10, constant: 10},
+		winston: { name: "Winston", health: 180, counter: 5, image: "assets/images/winston.png", attack: 5, constant: 5},
+		hanzo: { name: "Hanzo", health: 150, counter: 20, image: "assets/images/hanzo.png", attack: 7, constant: 7 },
+		soldier: { name: "Solider 76", health: 120, counter: 10, image: "assets/images/soldier.png", attack: 8, constant: 8 }
 	};
 
+	//Function that applies all the characters stats to their respective divs
 	$(".char").each(function(index, div){
 		var jDiv  = $(div);
  		var value = $(div).attr('value');
- 		jDiv.html("<h3>" + characters[value].name + "</h3>" +
+ 		jDiv.html("<p>" + characters[value].name + "</p>" +
+ 			"<img src=" + characters[value].image + " alt=" + characters[value].name + " />" +
  			"<p>Health: " + characters[value].health + "</p>" + 
- 			"<p>Attack: " + characters[value].counter + "</p>");
+ 			"<p>Attack: " + characters[value].attack + "</p>" +
+ 			"<p>Counter: " + characters[value].counter + "</p>");
 	})
 
+	//Function that determines the players "Hero"
 	$(".char").click(function() {
 		if( $("#enemies").is(":empty")){
 			var hero = $(this).addClass("hero");
 			$(this).removeClass("initial");
+			
+			//This function removes all the characters that weren't selected and adds the "enemy" class
 			if ($(".char").not("initial")) {
 				var enemies = $(".initial").addClass("enemy");
-				$("#enemies").append(enemies);
+				$("#enemies").hide().append(enemies).fadeIn(1000);
+				
+				//This allows the user to choose who to attack
 				$(".enemy").click(function(){
 					if ( $("#defender").is(":empty") ){
 						$(this).removeClass("enemy");
 						$(this).removeClass("initial")
 						$(this).addClass("defender");
 						var defender = $(this).addClass("defender");
-						$("#defender").append(defender);
+						$("#defender").hide().append(defender).fadeIn(1000);
 					}
 				});
 			}
 		}
 	});
+
+	//This function allows the user to attack the requested defender.
 	$("button").click(function(){
+			
+			//This checks to see if a defender has been selected. If no defender is present it will tell the user.
 			if ($("#defender").is(":empty") ){
 			alert("Please choose a defender");
 			}
+			
+			//If a defender is present then this code will run
 			else{
+				var value = $(".hero").attr('value');
+				var attackPower = characters[value].attack;
+				
+				//This will show how much damage the hero is dealing
 				$(".yourAttack").html("<p>You attack for " + attackPower + " damage!</p>");
+				var constant = characters[value].constant;
+				
+				//This section records the damage given and taken by the defender
 				$(".defender").each(function(index, div){
 					var value = $(div).attr('value');
 					var counter = characters[value].counter;
+					
+					//Will display how much damage the defender has given and update the characters stats accordingly
 					$(".enemyAttack").html("<p>" + characters[value].name + " attacks you for " + characters[value].counter + " damage!</p>");
 					characters[value].health -= attackPower;
 					console.log(characters[value].health)
 					var jDiv  = $(div);
- 					jDiv.html("<h3>" + characters[value].name + "</h3>" +
+ 					jDiv.html("<p>" + characters[value].name + "</p>" +
+ 					"<img src=" + characters[value].image + " alt=" + characters[value].name + " />" +
  					"<p>Health: " + characters[value].health + "</p>" + 
- 					"<p>Attack: " + characters[value].counter + "</p>");
+ 					"<p>Attack: " + characters[value].attack + "</p>" +
+ 					"<p>Counter: " + characters[value].counter + "</p>");
+ 					
+ 					//This will run once the defender has been defeated and prompts the user to choose someone else to attack.
  					if (characters[value].health <= 0){
- 						$(".defender").remove();
+ 						$(".hero").effect("shake");
  						$(".enemyAttack").empty();
- 						$(".yourAttack").html("<p>Good job! Select someone else to attack!");
+ 						$(".yourAttack").html("<p>Good job! Select the next defender!");
+						$(".defender").effect("explode", 1000).remove(); 						
+						
+						//If no characters are left to attack this code will run and let the user know they won.
+						if ($(".initial").length <= 0){
+							$(".defender").effect("explode").remove();
+							$(".enemeyAttack").remove();
+							$(".yourAttack").html("Congrats you win!");
+							alert("You Won!")
+							location.reload();
+						}
  					}
+
+ 					//This function updates the heroes health based on the attacker
 					$(".hero").each(function(index, div) {
 						var value = $(div).attr('value');
 						characters[value].health -= counter;
 						var jDiv  = $(div);
-	 					jDiv.html("<h3>" + characters[value].name + "</h3>" +
+	 					jDiv.html("<p>" + characters[value].name + "</p>" +
+	 					"<img src=" + characters[value].image + " alt=" + characters[value].name + " />" +
 	 					"<p>Health: " + characters[value].health + "</p>" + 
-	 					"<p>Attack: " + characters[value].counter + "</p>");
- 						/*if (characters[value].health <= 0){
+	 					"<p>Attack: " + characters[value].attack + "</p>" +
+	 					"<p>Counter: " + characters[value].counter + "</p>");
+ 						
+	 					//If the players health falls below 1 the game will end.
+ 						if (characters[value].health <= 0){
  							alert("Game Over :(");
- 							location.reload();*/
- 						//}
+ 							location.reload();
+ 						}
 					})
 				});
+
+				//The increases the players attack value each time the button is pressed.
+				characters[value].attack += constant;
 			}
-		attackPower += 8
 	});
-	if ($(".initial").length <= 0){
-		$(".enemeyAttack").empty();
-		$(".yourAttack").html("Congrats you win!");
-	}
 });
